969592e21ed42ee51d7d669c891e35e77ae22926
Mukta10 <muktapadamwar@gmail.com>
2024-07-03 17:14:32 +0530

    push

---
+++ BlockingStageQueue.java
@@ -0,0 +1,27 @@
+package dequeRepo;
+
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+
+
+public class BlockingStageQueue {
+	private final BlockingDeque<Item> queue;
+
+	
+	public BlockingStageQueue() {
+		this.queue = new LinkedBlockingDeque<>();
+	}
+
+	public void addItem(Item item) throws InterruptedException {
+		queue.put(item);
+	}
+
+	public Item pollItem() throws InterruptedException {
+		return queue.take();
+	}
+
+	
+	public boolean isEmpty() {
+		return queue.isEmpty();
+	}
+}
